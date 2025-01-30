687b2f8c22e620b5b01cf45e3a7e1fccea711d99
Manuel de Almeida <manuelalmeida70@outlook.pt>
2025-01-30 10:49:00 +0100

    Update prolog.pl, i change all code

--- prolog.pl
+++ prolog.pl
@@ -1,9 +1,2 @@
-% meu_programa.pl
-minha_regra(X) :- 
-    member(X, [1,2,3]),
-    X > 1.
-
-% Predicado para ser chamado do Haskell
-haskell_query(Input, Output) :-
-    % lógica aqui
-    Output is Input * 2.
+homem(mazunga).
+homem(jose).
